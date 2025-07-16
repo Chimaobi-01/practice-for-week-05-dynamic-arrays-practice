5684bd04d989e21b4752acdbeffa58941bdfc26d
Chinaka_Chimaobi <80992410+Chimaobi-01@users.noreply.github.com>
2025-07-16 19:39:39 +0000

    all green

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -3,17 +3,27 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
-
+    this.length = 0
+    this.capacity = defaultSize
+    this.data = new Array(this.capacity)
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index]
   }
 
   unshift(val) {
 
     // Your code here
+    for (let i = this.data.length - 1; i >= 0; i--) {
+      if(this.data[i] != undefined){
+        this.data[i+1] = this.data[i]
+      }
+    }
+    this.data[0] = val
+    this.length++
   }
 
 }
